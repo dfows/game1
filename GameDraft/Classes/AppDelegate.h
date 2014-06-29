1cae5086f3f32d84c76452e9f42d8c5f82c11636
jkwok91 <0111291@gmail.com>
2014-06-29 14:03:28 -0700

    Initial Commit

---
+++ GameDraft/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  GameDraft
+//
+//  Created by Jessica Kwok on 6/29/14.
+//  Copyright Jessica Kwok 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ GameDraft/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GameDraft
+//
+//  Created by Jessica Kwok on 6/29/14.
+//  Copyright Jessica Kwok 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
